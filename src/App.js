4bc63ddf80022d27ecce2f78f4738542fe86d79b
Cris Sepulveda <csepulveda7@knights.ucf.edu>
2020-10-09 22:39:57 -0400

    Set up node server

--- src/App.js
+++ src/App.js
@@ -3,48 +3,48 @@ import logo from './logo.svg';
 import './App.css';
 import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello Steven!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Click this totally safe link
-        </a>
-        <Component message = "Hello...?"/>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      greeting: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
 
-      </header>
-    </div>
-  );
-}
+  handleChange(event) {
+    this.setState({ name: event.target.value });
+  }
 
-class Component extends React.Component{
-  
-  constructor(props){
-    super(props)
-    this.state = {age: 20, height: 6} 
+  handleSubmit(event) {
+    event.preventDefault();
+    fetch(`/api/greeting?name=${encodeURIComponent(this.state.name)}`)
+      .then(response => response.json())
+      .then(state => this.setState(state));
   }
 
   render() {
     return (
-      <div>
-        {this.props.message}
-        {this.state.age}
-        <div style = {{backgroundColor: "red"}} onClick={() => this.setState({age: this.state.age + 1})}>
-          button
-        </div>
+      <div className="App">
+        <header className="App-header">
+          <form onSubmit={this.handleSubmit}>
+            <label htmlFor="name">Enter your name: </label>
+            <input
+              id="name"
+              type="text"
+              value={this.state.name}
+              onChange={this.handleChange}
+            />
+            <button type="submit">Submit</button>
+          </form>
+          <p>{this.state.greeting}</p>
+        </header>
       </div>
     );
   }
 }
 
-
 export default App;
+
