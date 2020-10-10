f79cbc21d21c917bed7985eb88df0a084626f7f6
Jordan Schneider <60559211+schneidercorn@users.noreply.github.com>
2020-10-10 05:48:01 -0400

    Setup panels, clean up unused objects, design cleanliness

--- src/VRScene.js
+++ src/VRScene.js
@@ -2,10 +2,49 @@ import 'aframe';
 import 'aframe-animation-component';
 import 'aframe-particle-system-component';
 import 'babel-polyfill';
-import Skybox from './assets/Skyboxes/GrandCentralStation.jpg';
+import Skybox from './assets/Skyboxes/TimesSquare.jpg';
+import PanelDesign from './assets/PanelDesign/PanelDesign.png';
 import {Entity, Scene} from 'aframe-react';
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+{/**/}
+
+{/*Set Panels rotation and position*/}
+const radius = 10;
+const positions = []
+
+let degrees = 0
+let rotation = -90
+
+for (let i = 0; i < 8; i++) {
+    let xPos = Math.cos(degrees) * radius
+    let zPos = Math.sin(degrees) * radius
+
+    let orientation = {}
+
+    orientation.position = `${xPos} -2 ${zPos}`
+    orientation.rotation = `-10 ${rotation} 0`
+    positions.push(orientation)
+
+    orientation = {}
+
+    orientation.position = `${xPos} 3 ${zPos}`
+    orientation.rotation = `-10 ${rotation} 0`
+    positions.push(orientation)
+
+    orientation = {}
+
+    orientation.position = `${xPos} 8 ${zPos}`
+    orientation.rotation = `-10 ${rotation} 0`
+    positions.push(orientation)
+
+    degrees += Math.PI/4
+    rotation -= 45
+}
+
+
+
 
 class VRScene extends React.Component {
   constructor(props) {
@@ -13,43 +52,34 @@ class VRScene extends React.Component {
     this.state = {color: 'red'};
   }
 
-  changeColor() {
-    const colors = ['red', 'orange', 'yellow', 'green', 'blue'];
-    this.setState({
-      color: colors[Math.floor(Math.random() * colors.length)]
-    });
-  }
-
   render () {
     return (
-      <Scene>
-        <a-assets> // store assets
-          <img id="groundTexture" src="https://cdn.aframe.io/a-painter/images/floor.jpg"/>
+      <Scene renderer=
+      "antialias: true"
+      >
+        <a-assets>
+          {/*Skybox*/}
           <img id="skyTexture" src={Skybox}/>
+          <img id="tweetTexture" src={PanelDesign}/>
         </a-assets>
 
-
-        <Entity primitive="a-light" type="ambient" color="#445451"/>
-        <Entity primitive="a-light" type="point" intensity="2" position="2 4 4"/>
         <Entity primitive="a-sky" src="#skyTexture" rotation="0 -130 0"/>
-        <Entity particle-system={{preset: 'snow', particleCount: 2000}}/>
-        <Entity text={{value: 'Hello, A-Frame React!', align: 'center'}} position={{x: 0, y: 2, z: -1}}/>
 
-        <Entity id="box"
-          geometry={{primitive: 'box'}}
-          material={{color: this.state.color, opacity: 0.6}}
-          animation__rotate={{property: 'rotation', dur: 2000, loop: true, to: '360 360 360'}}
-          animation__scale={{property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '1.1 1.1 1.1'}}
-          position={{x: 0, y: 1, z: -3}}
-          events={{click: this.changeColor.bind(this)}}>
-          <Entity animation__scale={{property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '2 2 2'}}
-                  geometry={{primitive: 'box', depth: 0.2, height: 0.2, width: 0.2}}
-                  material={{color: '#24CAFF'}}/>
-        </Entity>
-
-        <Entity primitive="a-camera">
-          <Entity primitive="a-cursor" animation__click={{property: 'scale', startEvents: 'click', from: '0.1 0.1 0.1', to: '1 1 1', dur: 150}}/>
-        </Entity>
+        {/*Tweet Panels*/}
+        {
+          positions.map(({rotation, position}) => (
+            <Entity
+              primitive="a-plane"
+              src="#tweetTexture"
+              material= {{opacity: 0.99}}
+              rotation={rotation}
+              position={position}
+              scale="5 3"
+              event-set__mouseenter="scale: 1.2 1.2 1"
+              event-set__mouseleave="scale: 1 1 1"
+            />
+          ))
+        }
       </Scene>
     );
   }
