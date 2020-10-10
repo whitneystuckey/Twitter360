493d116562855448ca04a51c9e598f96bae36e66
Cris Sepulveda <csepulveda7@knights.ucf.edu>
2020-10-10 18:26:22 -0400

    added tweet data and markers/buttons

---
+++ src/data/twitterData.js
@@ -0,0 +1,137 @@
+export const twitterData = [
+  {
+    country: "Japan",
+    city: "Tokyo",
+    lat: 35.6897,
+    long: 139.6922,
+    total: 37977000,
+    tweet: "Tonight, @FLOTUS and I tested positive for COVID-19. We will begin our quarantine and recovery process immediately. We will get through this TOGETHER!",
+    id: 1
+  },
+  {
+    country: "Indonesia",
+    city: "Jakarta",
+    lat: -6.2146,
+    long: 106.8451,
+    total: 37977000,
+    tweet: "Tonight, @FLOTUS and I tested positive for COVID-19. We will begin our quarantine and recovery process immediately. We will get through this TOGETHER!",
+    id: 2
+  },
+  {
+    country: "India",
+    city: "Delhi",
+    lat: 28.66,
+    long: 77.23,
+    total: 37977000,
+    tweet: "Tonight, @FLOTUS and I tested positive for COVID-19. We will begin our quarantine and recovery process immediately. We will get through this TOGETHER!",
+    id: 3
+  },
+  {
+    country: "India",
+    city: "Mumbai",
+    lat: 18.9667,
+    long: 72.8333,
+    total: 37977000,
+    tweet: "Tonight, @FLOTUS and I tested positive for COVID-19. We will begin our quarantine and recovery process immediately. We will get through this TOGETHER!",
+    id: 4
+  },
+  {
+    country: "Philippines",
+    city: "Manila",
+    lat: 14.5958,
+    long: 120.9772,
+    total: 37977000,
+    tweet: "Tonight, @FLOTUS and I tested positive for COVID-19. We will begin our quarantine and recovery process immediately. We will get through this TOGETHER!",
+    id: 5
+  },
+  {
+    country: "China",
+    city: "Shanghai",
+    lat: 31.1667,
+    long: 121.4667,
+    total: 37977000,
+    tweet: "Tonight, @FLOTUS and I tested positive for COVID-19. We will begin our quarantine and recovery process immediately. We will get through this TOGETHER!",
+    id: 6
+  },
+  {
+    country: "Brazil",
+    city: "São Paulo",
+    lat: -23.5504,
+    long: -46.6339,
+    total: 37977000,
+    tweet: "Tonight, @FLOTUS and I tested positive for COVID-19. We will begin our quarantine and recovery process immediately. We will get through this TOGETHER!",
+    id: 7
+  },
+  {
+    country: "Korea, South",
+    city: "Seoul",
+    lat: 37.5833,
+    long: 127,
+    total: 37977000,
+    tweet: "Tonight, @FLOTUS and I tested positive for COVID-19. We will begin our quarantine and recovery process immediately. We will get through this TOGETHER!",
+    id: 8
+  },
+  {
+    country: "Mexico",
+    city: "Mexico City",
+    lat: 19.4333,
+    long: -99.1333,
+    total: 37977000,
+    tweet: "Tonight, @FLOTUS and I tested positive for COVID-19. We will begin our quarantine and recovery process immediately. We will get through this TOGETHER!",
+    id: 9
+  },
+  {
+    country: "China",
+    city: "Guangzhou",
+    lat: 23.1288,
+    long: 113.259,
+    total: 37977000,
+    tweet: "Tonight, @FLOTUS and I tested positive for COVID-19. We will begin our quarantine and recovery process immediately. We will get through this TOGETHER!",
+    id: 10
+  },
+  {
+    country: "China",
+    city: "Beijing",
+    lat: 39.905,
+    long: 116.3914,
+    total: 37977000,
+    tweet: "Tonight, @FLOTUS and I tested positive for COVID-19. We will begin our quarantine and recovery process immediately. We will get through this TOGETHER!",
+    id: 11
+  },
+  {
+    country: "Egypt",
+    city: "Cairo",
+    lat: 30.0561,
+    long: 31.2394,
+    total: 37977000,
+    tweet: "Tonight, @FLOTUS and I tested positive for COVID-19. We will begin our quarantine and recovery process immediately. We will get through this TOGETHER!",
+    id: 12
+  },
+  {
+    country: "United States",
+    city: "New York",
+    lat: 40.6943,
+    long: -73.9249,
+    total: 37977000,
+    tweet: "Tonight, @FLOTUS and I tested positive for COVID-19. We will begin our quarantine and recovery process immediately. We will get through this TOGETHER!",
+    id: 13
+  },
+  {
+    country: "India",
+    city: "Kolkāta",
+    lat: 22.5411,
+    long: 88.3378,
+    total: 37977000,
+    tweet: "Tonight, @FLOTUS and I tested positive for COVID-19. We will begin our quarantine and recovery process immediately. We will get through this TOGETHER!",
+    id: 14
+  },
+  {
+    country: "Russia",
+    city: "Moscow",
+    lat: 55.7558,
+    long: 37.6178,
+    total: 37977000,
+    tweet: "Tonight, @FLOTUS and I tested positive for COVID-19. We will begin our quarantine and recovery process immediately. We will get through this TOGETHER!",
+    id: 15
+  },
+]
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,15 @@
 import React, { useState } from 'react';
 import './pages.css';
-import ReactMapGL from "react-map-gl"
+import ReactMapGL, { Marker } from "react-map-gl";
+import { twitterData } from '../data/twitterData';
 
 export default function Home() {
     const [viewport, setViewport] = useState({
-        width: '100vw',
+        width: '80vw',
         height: '100vh',
-        latitude: 45.4211,
-        longitude: -75.6903,
-        zoom: 10
+        latitude: 28.6011,
+        longitude: -81.2004,
+        zoom: 3
     });
 
     return (
@@ -16,9 +17,26 @@ export default function Home() {
             <ReactMapGL 
                 { ...viewport }
                 mapboxApiAccessToken = { process.env.REACT_APP_MAPBOX_TOKEN }
+                mapStyle = 'mapbox://styles/csepulveda7/ckg41stxq2aec1anzf925xogi'
+                onViewportChange={ viewport => {
+                    setViewport(viewport);
+                }}
             >
-                markers here
+                { twitterData.map((hotspots) => (
+                    <Marker 
+                        key={ hotspots.id } 
+                        latitude = { hotspots.lat } 
+                        longitude = { hotspots.long }
+                    >
+                        <button className="markerBtn">
+                            <img className="markers" src={require("../assets/logo.png")}/>
+                        </button>
+                    </Marker>
+                ))}
             </ReactMapGL>
+            <div className = 'sidebar'>
+                Sidebar
+            </div>
         </div>
     )
 }
