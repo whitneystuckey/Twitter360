c35902bac4185a41e512a15552bb28f6a29cc220
Jordan Schneider <60559211+schneidercorn@users.noreply.github.com>
2020-10-10 00:28:41 -0400

    360 Image Updated

--- src/VRScene.js
+++ src/VRScene.js
@@ -2,6 +2,7 @@ import 'aframe';
 import 'aframe-animation-component';
 import 'aframe-particle-system-component';
 import 'babel-polyfill';
+import Skybox from './assets/Skyboxes/GrandCentralStation.jpg';
 import {Entity, Scene} from 'aframe-react';
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -22,15 +23,15 @@ class VRScene extends React.Component {
   render () {
     return (
       <Scene>
-        <a-assets>
+        <a-assets> // store assets
           <img id="groundTexture" src="https://cdn.aframe.io/a-painter/images/floor.jpg"/>
-          <img id="skyTexture" src="https://cdn.aframe.io/a-painter/images/sky.jpg"/>
+          <img id="skyTexture" src={Skybox}/>
         </a-assets>
 
-        <Entity primitive="a-plane" src="#groundTexture" rotation="-90 0 0" height="100" width="100"/>
+
         <Entity primitive="a-light" type="ambient" color="#445451"/>
         <Entity primitive="a-light" type="point" intensity="2" position="2 4 4"/>
-        <Entity primitive="a-sky" height="2048" radius="30" src="#skyTexture" theta-length="90" width="2048"/>
+        <Entity primitive="a-sky" src="#skyTexture" rotation="0 -130 0"/>
         <Entity particle-system={{preset: 'snow', particleCount: 2000}}/>
         <Entity text={{value: 'Hello, A-Frame React!', align: 'center'}} position={{x: 0, y: 2, z: -1}}/>
 
